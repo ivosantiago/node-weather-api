e1e0e24ff069719470727dc6af2d300810a0114a
Ivo Santiago <ivosantiago@gmail.com>
2020-08-24 10:51:23 -0300

    Aula 09

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -39,7 +39,12 @@ export class StormGlass {
 
   public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
     const response = await this.request.get(
-      `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&end=1592113802`
+      `https://api.stormglass.io/v2/weather/point?lat=${lat}&lng=${lng}&params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&end=1592113802`,
+      {
+        headers: {
+          Authorization: 'fake-token',
+        },
+      }
     );
 
     return this.normalizeResponse(response.data);
